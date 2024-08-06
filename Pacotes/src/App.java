f7ee9b945223b144649e3fcc5570febca4b24a91
Cabr1to <luis.b.c.melo@gmail.com>
2024-08-06 13:27:29 -0300

    pacotes added

---
+++ Pacotes/src/App.java
@@ -0,0 +1,18 @@
+import gui.MiniaturaVideo;
+import videos.*;
+
+
+public class App {
+    public static void main(String[] args) throws Exception {
+        Filme filme1 = new Filme();
+
+        Anime anime = new Anime();
+
+        Serie serie = new Serie();
+
+        Documentario documentario = new Documentario();
+
+        MiniaturaVideo 
+
+    }
+}
